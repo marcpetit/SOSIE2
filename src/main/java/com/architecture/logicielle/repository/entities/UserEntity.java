204d063702e84a23269db489d3f9a9b62ff9ca6e
Obitanz <obitanz@netbook>
2017-10-16 10:38:08 +0200

    Sprint 2 - Github - exemple de pull-request

---
+++ src/main/java/com/architecture/logicielle/repository/entities/UserEntity.java
@@ -0,0 +1,118 @@
+package com.architecture.logicielle.repository.entities;
+
+import java.io.File;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "user")
+@Entity // This tells Hibernate to make a table out of this class
+public class UserEntity {
+	
+	public UserEntity() {
+		
+	}
+	
+	public UserEntity(String firstName, String lastName, String statut, String mail, String password, File photo, Long username) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.statut = statut;
+		this.email = mail;
+		this.password = password;
+		this.photo = photo;
+		this.username = username;
+	}
+
+	@Column(name = "firstName")
+    private String firstName;
+
+	@Column(name = "lastName")
+    private String lastName;
+	
+	@Id
+	@Column(name = "username")
+	private Long username;
+	
+	@Column(name = "statut")
+    private String statut;
+
+    @Column(name = "email")
+    private String email;
+    
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "photo")
+    private File photo;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getStatut() {
+		return statut;
+	}
+
+	public void setStatut(String statut) {
+		this.statut = statut;
+	}
+
+	public String getMail() {
+		return email;
+	}
+
+	public void setMail(String mail) {
+		this.email = mail;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public File getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(File photo) {
+		this.photo = photo;
+	}
+
+	@Override
+	public String toString() {
+		return "User [firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+
+	public Long getUsername() {
+		return username;
+	}
+
+	public void setUsername(Long username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
---
+++ src/main/java/com/architecture/logicielle/service/UserServiceImpl.java
@@ -0,0 +1,62 @@
+package com.architecture.logicielle.service;
+
+import com.architecture.logicielle.mvc.data.UserView;
+import com.architecture.logicielle.repository.UserRepository;
+import com.architecture.logicielle.repository.entities.UserEntity;
+
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public UserEntity parseUserViewToUserEntity(UserView userview) {
+
+		UserEntity userEnt = new UserEntity();
+		userEnt.setFirstName(userview.getFirstName());
+		userEnt.setLastName(userview.getLastName());
+		userEnt.setUsername(userview.getUsername());
+		userEnt.setMail(userview.getMail());
+		userEnt.setPassword(userview.getPassword());
+		userEnt.setPhoto(userview.getPhoto());
+		userEnt.setStatut(userview.getStatut());
+
+		return userEnt;
+	}
+
+	@Override
+	public void saveUser(UserEntity userEntity, UserRepository userRepository) {
+		userRepository.save(userEntity);
+	}
+
+	@Override
+	public UserEntity checkUser(UserEntity userEntity, UserRepository userRepository) {
+		UserEntity user = new UserEntity();
+		user = userRepository.findOne(userEntity.getUsername());
+		return user;
+	}
+
+	@Override
+	public UserEntity GetUserById(Long id, UserRepository userRepository) {
+		return userRepository.findOne(id);
+		
+	}
+
+	@Override
+	public UserView parseUserEntityToUserView(UserEntity userentity) {
+
+		UserView userview = new UserView();
+		userview.setFirstName(userentity.getFirstName());
+		userview.setLastName(userentity.getLastName());
+		userview.setUsername(userentity.getUsername());
+		userview.setMail(userentity.getMail());
+		userview.setPassword(userentity.getPassword());
+		userview.setPhoto(userentity.getPhoto());
+		userview.setStatut(userentity.getStatut());
+
+		return userview;
+	}
+
+	@Override
+	public void deleteUser(UserEntity userEntity, UserRepository userRepository) {
+		userRepository.delete(userEntity);
+	}
+
+}
